ad766079a753e2f95664c16123325e8909b07b82
shitsurae-lab <20566363+shitsurae-lab@users.noreply.github.com>
2022-02-17 20:24:58 +0900

    Checked Local.

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <NuxtLogo />
+    <Nuxt />
+  </div>
+</template>
+<script>
+import NuxtLogo from '../components/NuxtLogo.vue'
+export default {
+  data() {
+    return {}
+  },
+  components: { NuxtLogo },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>{{ `${title}` }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      title: 'This is Top page',
+    }
+  },
+}
+</script>
