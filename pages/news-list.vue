e4e47c820dc6a440a1ffb4975de478f7dd1e6c1b
shitsurae-lab <20566363+shitsurae-lab@users.noreply.github.com>
2022-02-18 17:15:29 +0900

    記事一覧ページをnews-list.vueとして制作

---
+++ pages/news-list.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  async asyncData() {
+    const { data } = await axios.get(
+      `https://shitsurae104.microcms.io/api/v1/portfolio`,
+      {
+        headers: {
+          'X-MICROCMS-API-KEY': 'ec3af9e4779a494bbc2a9a054a35ad1dceae',
+        },
+      }
+    )
+    return data
+  },
+}
+</script>
