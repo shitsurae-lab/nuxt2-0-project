8eb67a05289c6a46f945c7b83c2f4ddaccf3aca4
shitsurae-lab <20566363+shitsurae-lab@users.noreply.github.com>
2022-02-18 17:14:05 +0900

    記事はpages直下に'_slug'ディレクトリを配置、その中にindex.vueを記述

---
+++ pages/_slug/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <main class="main">
+    <h1 class="title">{{ `${title}` }}</h1>
+    <p class="publishedAt">{{ publishedAt }}</p>
+    <div class="post" v-html="body"></div>
+  </main>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData({ params }) {
+    const { data } = await axios.get(
+      `https://shitsurae104.microcms.io/api/v1/portfolio/${params.slug}`,
+      {
+        headers: {
+          'X-MICROCMS-API-KEY': 'ec3af9e4779a494bbc2a9a054a35ad1dceae',
+        },
+      }
+    )
+    return data
+  },
+}
+</script>
