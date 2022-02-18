f38985bac27c74a8c3c103f0fe2f75219e5bea3c
shitsurae-lab <20566363+shitsurae-lab@users.noreply.github.com>
2022-02-18 17:14:42 +0900

    navにNewsを追加

--- components/Nav.vue
+++ components/Nav.vue
@@ -2,5 +2,6 @@
   <ul>
     <li><nuxt-link to="/">Home</nuxt-link></li>
     <li><nuxt-link to="/about">About</nuxt-link></li>
+    <li><nuxt-link to="/news-list">News</nuxt-link></li>
   </ul>
 </template>
